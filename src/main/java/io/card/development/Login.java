a0d1495a2a4991f5c512da74757478c231df99aa
Deane <d4rracuda@ya.ru>
2019-03-31 04:59:37 +0300

    fff

--- src/main/java/io/card/development/Login.java
+++ src/main/java/io/card/development/Login.java
@@ -2,62 +2,60 @@ package io.card.development;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class Login extends AppCompatActivity {
 
     public static final String APP_PREFERENCES = "mysettings";
+
+    private String userPass;
 
     public static final String APP_PREFERENCES_NAME = "Pin";
     SharedPreferences mSettings;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        mSettings = getSharedPreferences(APP_PREFERENCES, Context.MODE_PRIVATE);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
         SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(Login.this);
-        String pass = sharedPreferences.getString("passwordStart", null);
+        final String pass = sharedPreferences.getString("passwordStart", null);
         if(pass == null){
-           ifFirstStart();
+           FirstStart();
         } else {
-            // тут получение пароля (обработка кнопки) и проверка пароля с переменной pass
+            final EditText editPin = (EditText)findViewById(R.id.editText);
+
+
+            Button signIn = (Button) findViewById(R.id.button);
+            signIn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    userPass = editPin.getText().toString().trim();
+                    if(userPass.equals(pass)){
+                        FirstStart();
+                    } else {
+                        Toast toast = Toast.makeText(getApplicationContext(),
+                                "Pin неверный", Toast.LENGTH_SHORT);
+                        toast.show();
+                    }
+                }
+            });
         }
     }
 
-    private void ifFirstStart(){
+    private void FirstStart() {
+        // открытие SampleActivity
+        Intent intent = new Intent(Login.this, SampleActivity.class);
+        startActivity(intent);
 
-        class SharedPreferencesExample extends Activity {
-        private static final String MY_SETTINGS = "my_settings";
-
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_login);
-
-            SharedPreferences sp = getSharedPreferences(MY_SETTINGS,
-                    Context.MODE_PRIVATE);
-            // проверяем, первый ли раз открывается программа
-            boolean hasVisited = sp.getBoolean("hasVisited", false);
-
-            if (!hasVisited) {
-                // выводим нужную активность
-                SharedPreferences.Editor e = sp.edit();
-                e.putBoolean("hasVisited", true);
-                e.commit(); // не забудьте подтвердить изменения
-            }
-        }
-
-    }
-
-    private void checkPass(){
-        EditText editPin = (EditText)findViewById(R.id.editPin);
-        String strNickName = editPin.getText().toString();
     }
 }
